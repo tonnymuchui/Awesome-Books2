1b6819435483145d2e6b52ce904a052a10847ead
tonnymuchui <tonykanyingah@gmail.com>
2022-10-12 18:49:29 +0300

    Add book object module file

---
+++ modules/BookObject.js
@@ -0,0 +1,27 @@
+export default class BookObject {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
+
+  static addBook(book) {
+    const books = BookObject.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(author) {
+    const books = BookObject.getBooks();
+    books.forEach((book, index) => {
+      if (book.author === author) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
